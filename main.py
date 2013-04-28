ee8ad4054e951ef9598e08d4d8852fdd065ffcd7
Jackson Okuhn <jacksonokuhn@college.harvard.edu>
2013-04-28 16:25:46 -0400

    Bug Fixes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import image
 
 im = image.sc_Image.from_filepath2("skateboarder.jpg")
 
-im.shrink(100, alg = 'dyn')
+im.shrink(100, alg = 'dyn', energy = "e1_five")
 # image.shrink( 'horizontal', 300 , 'e1', 'dyn')
 
 im.to_jpeg("new_skateboarder2.jpg")
