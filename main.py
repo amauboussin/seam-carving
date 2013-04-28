7775223e644abfa4b909509c9c3d1256d61a4d01
Andrew Mauboussin <ammaub@college.harvard.edu>
2013-04-28 17:48:43 -0400

    wrote readme

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import image
 
 im = image.sc_Image.from_filepath2("comet.jpg")
 
-im.to_energy_pic('comet_energy2.jpg')
-im.shrink(50, alg = 'dyn', orientation = 'vertical')
+im.enlarge(2, alg = 'dyn', orientation = 'vertical')
 # image.shrink( 'horizontal', 300 , 'e1', 'dyn')
 
-im.to_jpeg("comet_vert.jpg")
+im.to_jpeg("comet_big.jpg")
 
