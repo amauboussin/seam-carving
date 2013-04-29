4b26b1e746745a3128dd8c9d989a34e3b6124115
Andrew Mauboussin <ammaub@college.harvard.edu>
2013-04-28 20:37:14 -0400

    finished image enlargement

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import image
 
-im = image.sc_Image.from_filepath2("comet.jpg")
+im = image.sc_Image.from_filepath2("skateboarder.jpg")
 
-im.enlarge(2, alg = 'dyn', orientation = 'vertical')
+im.enlarge(80, alg = 'dyn', orientation = 'vertical')
 # image.shrink( 'horizontal', 300 , 'e1', 'dyn')
 
-im.to_jpeg("comet_big.jpg")
+im.to_jpeg("skateboarder_big.jpg")
 
